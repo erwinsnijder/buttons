71a701520ab46e12d6ec19705b6d396c88c4d1db
erwinsnijder <48627520+erwinsnijder@users.noreply.github.com>
2019-05-07 09:48:32 +0200

    calc

---
+++ button.js
@@ -0,0 +1,12 @@
+var test = document.getElementById('1');
+test.onclick = function() {
+    console.log('1');
+}
+var test = document.getElementById('2');
+test.onclick = function() {
+    console.log('2');
+}
+var test = document.getElementById('3');
+test.onclick = function() {
+    console.log('3');
+}

4b86fadaec89723ce00f3285d4917345729ea85e
erwinsnijder <48627520+erwinsnijder@users.noreply.github.com>
2019-05-07 10:33:28 +0200

    Update button.js

--- button.js
+++ button.js
@@ -1,17 +1,24 @@
 var test = document.getElementById('1');
 test.onclick = function() {
-    console.log('1');
+    document.getElementById('output').innerHTML += "1"
 }
 var test = document.getElementById('2');
 test.onclick = function() {
-    console.log('2');
+    document.getElementById('output').innerHTML += "2"
 }
 var test = document.getElementById('3');
 test.onclick = function() {
-    console.log('3');
+    document.getElementById('output').innerHTML += "3"
 }
 var test = document.getElementById('+');
 test.onclick = function() {
-    document.getElementById('output')
+    document.getElementById('output').innerHTML += "+"
+
+}
+var test = document.getElementById('=');
+test.onclick = function() {
+   var x = eval(document.getElementById('output').innerHTML);
+   document.getElementById('output').innerHTML = x;
+   console.log(x);
 
 }

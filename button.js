22c472f81f8c7fbdb9f86bb3e4dc4987a76408ef
erwinsnijder <48627520+erwinsnijder@users.noreply.github.com>
2019-05-08 12:43:45 +0200

    buttons
    added more buttons
    added basic grid

--- button.js
+++ button.js
@@ -10,6 +10,34 @@ var test = document.getElementById('3');
 test.onclick = function() {
     document.getElementById('output').innerHTML += "3"
 }
+var test = document.getElementById('4');
+test.onclick = function() {
+    document.getElementById('output').innerHTML += "4"
+}
+var test = document.getElementById('5');
+test.onclick = function() {
+    document.getElementById('output').innerHTML += "5"
+}
+var test = document.getElementById('6');
+test.onclick = function() {
+    document.getElementById('output').innerHTML += "6"
+}
+var test = document.getElementById('7');
+test.onclick = function() {
+    document.getElementById('output').innerHTML += "7"
+}
+var test = document.getElementById('8');
+test.onclick = function() {
+    document.getElementById('output').innerHTML += "8"
+}
+var test = document.getElementById('9');
+test.onclick = function() {
+    document.getElementById('output').innerHTML += "9"
+}
+var test = document.getElementById('0');
+test.onclick = function() {
+    document.getElementById('output').innerHTML += "0"
+}
 var test = document.getElementById('+');
 test.onclick = function() {
     document.getElementById('output').innerHTML += "+"
